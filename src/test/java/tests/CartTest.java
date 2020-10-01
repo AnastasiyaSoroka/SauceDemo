f8e6b2c8717aa3172152b6d5a8e9537509b68ab5
AnastasiyaSoroka <Lasertag89>
2020-10-01 20:55:41 +0300

    Added CartTest

---
+++ src/test/java/tests/CartTest.java
@@ -0,0 +1,20 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CartTest extends Settings {
+
+
+    @Test
+    public void productShouldBeAddedIntoCart() {
+        loginPage.openPage();
+        loginPage.login(username, password);
+        productPage.addToCart("Sauce Labs Fleece Jacket");
+        cartPage.openPage();
+
+        Assert.assertEquals(cartPage.getPriceForProduct("Sauce Labs Fleece Jacket"), "49.99");
+        Assert.assertEquals(cartPage.getQuantityForProduct("Sauce Labs Fleece Jacket"), "1");
+    }
+
+}
