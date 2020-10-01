52af9354fb69ebc28a4e4064fac841dd7aa2bbda
AnastasiyaSoroka <Lasertag89>
2020-10-01 20:56:15 +0300

    Added CheckoutOverviewTest

---
+++ src/test/java/tests/CheckoutOverviewTest.java
@@ -0,0 +1,23 @@
+package tests;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class CheckoutOverviewTest extends Settings {
+
+    @Test
+    public void checkCheckoutOverviewPageOpened() {
+        loginPage.openPage();
+        loginPage.login(username, password);
+        productPage.addToCart("Sauce Labs Fleece Jacket");
+        cartPage.openPage();
+        cartPage.clickCheckout();
+        checkoutPage.sendKeysToFirstName("blablabla");
+        checkoutPage.sendKeysToLastName("blablabla");
+        checkoutPage.sendKeysToZipCode("111jjj333");
+        checkoutPage.clickContinue();
+
+        Assert.assertEquals(checkOutOverviewPage.getCurrentURL(), "https://www.saucedemo.com/checkout-step-two.html", "CheckOut:Overview page should be opened");
+    }
+
+}
